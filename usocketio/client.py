7ba6a0fe19bc4983784a84c88ec3aa450ca5eb10
omelancon <ol.melancon@gmail.com>
2023-02-22 18:03:03 -0500

    add https support

--- usocketio/client.py
+++ usocketio/client.py
@@ -5,6 +5,7 @@ Micropython Socket.IO client.
 import logging
 import ure as re
 import ujson as json
+import ussl as ssl
 import usocket as socket
 from ucollections import namedtuple
 
@@ -13,15 +14,49 @@ from .transport import SocketIO
 
 LOGGER = logging.getLogger(__name__)
 
-URL_RE = re.compile(r'http://([A-Za-z0-9\-\.]+)(?:\:([0-9]+))?(/.+)?')
-URI = namedtuple('URI', ('hostname', 'port', 'path'))
+URL_RE = re.compile(r'(https?)://([A-Za-z0-9\-\.]+)(?:\:([0-9]+))?(/.+)?')
+URI = namedtuple('URI', ('protocol', 'hostname', 'port', 'path'))
 
 
 def urlparse(uri):
     """Parse http:// URLs"""
     match = URL_RE.match(uri)
     if match:
-        return URI(match.group(1), int(match.group(2)), match.group(3))
+        return URI(match.group(1), match.group(2), int(match.group(3)), match.group(4))
+
+
+def _connect(sock, hostname, port, path):
+    def send_header(header, *args):
+        if __debug__:
+            LOGGER.debug(str(header), *args)
+
+        sock.write(header % args + '\r\n')
+
+    send_header(b'GET %s HTTP/1.1', path)
+    send_header(b'Host: %s:%s', hostname, port)
+    send_header(b'')
+
+    header = sock.readline()[:-2]
+    assert header == b'HTTP/1.1 200 OK', header
+
+    length = None
+
+    while header:
+        header = sock.readline()[:-2]
+        if not header:
+            break
+
+        header, value = header.split(b': ')
+        header = header.lower()
+        if header == b'content-type':
+            assert value == b'application/octet-stream'
+        elif header == b'content-length':
+            length = int(value)
+
+    assert length
+
+    data = sock.read(length)
+    return decode_payload(data)
 
 
 def _connect_http(hostname, port, path):
@@ -30,54 +65,41 @@ def _connect_http(hostname, port, path):
         sock = socket.socket()
         addr = socket.getaddrinfo(hostname, port)
         sock.connect(addr[0][4])
-
-        def send_header(header, *args):
-            if __debug__:
-                LOGGER.debug(str(header), *args)
-
-            sock.write(header % args + '\r\n')
-
-        send_header(b'GET %s HTTP/1.1', path)
-        send_header(b'Host: %s:%s', hostname, port)
-        send_header(b'')
-
-        header = sock.readline()[:-2]
-        assert header == b'HTTP/1.1 200 OK', header
-
-        length = None
-
-        while header:
-            header = sock.readline()[:-2]
-            if not header:
-                break
-
-            header, value = header.split(b': ')
-            header = header.lower()
-            if header == b'content-type':
-                assert value == b'application/octet-stream'
-            elif header == b'content-length':
-                length = int(value)
-
-        assert length
-
-        data = sock.read(length)
-        return decode_payload(data)
-
+        return _connect(sock, hostname, port, path)
     finally:
         sock.close()
 
 
-def connect(uri):
+def _connect_https(hostname, port, path):
+    """Stage 1 do the HTTPS connection to get our SID"""
+    try:
+        sock = socket.socket()
+        addr = socket.getaddrinfo(hostname, port)
+        ssock = ssl.wrap_socket(sock)
+        ssock.connect(addr[0][4])
+        return _connect(ssock, hostname, port, path)
+    finally:
+        ssock.close()
+
+
+def connect(uri, query=""):
     """Connect to a socket IO server."""
     uri = urlparse(uri)
 
     assert uri
 
-    path = uri.path or '/' + 'socket.io/?EIO=3'
+    path = uri.path or '/' + 'socket.io/?EIO=4'
 
-    # Start a HTTP connection, which will give us an SID to use to upgrade
+    if query:
+        path += "&" + query
+
+    # Start a connection, which will give us an SID to use to upgrade
     # the websockets connection
-    packets = _connect_http(uri.hostname, uri.port, path)
+    if path.protocol == 'http':
+        packets = _connect_http(uri.hostname, uri.port, path)
+    else:
+        packets = _connect_https(uri.hostname, uri.port, path)
+
     # The first packet should open the connection,
     # following packets might be initialisation messages for us
     packet_type, params = next(packets)
